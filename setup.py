ba96d72fea02115481f739d5682dd7ef16c86f1f
chuck <chuckdustin12@gmail.com>
2023-06-13 16:50:00 -0500

    fixed imports

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/chuckdustin12/polygonsdk/",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3.11.4",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
