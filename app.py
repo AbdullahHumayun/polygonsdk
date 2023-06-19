93ffb7509b1a2fdf1cd7164c9c1f9493574b45bc
chuck <chuckdustin12@gmail.com>
2023-06-18 20:37:03 -0500

    fixed imports

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ import pandas as pd
 import aiohttp
 from flask import Flask, render_template, request, jsonify
 from api_master.cfg import YOUR_API_KEY
-from api_master.funcs.get_data import get_webull_data
-from api_master.sdks.stocksera_sdk.sdk import StockSeraSDK
+from api_master.examples.get_data import get_webull_data
+from api_master.sdks.stocksera_sdk.sdk import StockeraSDK
 from api_master.sdks.webull_sdk.webull_sdk import AsyncWebullSDK
 from static.py.api_functions import get_top_gainers_data, volume_analysis_endpoint, financial_statement_endpoint,balance_sheet_endpoint, cash_flow_endpoint, balance_sheet_endpoint
 from static.py.api_functions import financial_ratios_endpoint, capital_flow_endpoint, process_data, institutional_holdings_endpoint,short_interest_endpoint
 from static.py.api_functions import analyst_ratings_endpoint, stock_data_endpoint,get_top_gainers_data, earnings_calendar_endpoint, get_fifty_twos_endpoint
 
 import asyncio
-_stocksera = StockSeraSDK()
+_stocksera = StockeraSDK()
 webull = AsyncWebullSDK()
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
